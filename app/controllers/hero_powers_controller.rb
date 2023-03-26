5ac4dfd0432e7bf9ec1d34f819734991dffbe61e
son189 <son.august.7334@gmail.com>
2023-03-26 21:12:47 +0300

    few bugs

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,8 +1,12 @@
 class HeroPowersController < ApplicationController
     def create
         heropower = Heropower.create(heropower_params)
+        if heropower.save
         render json: heropower.hero
+        else 
+            render json: { error: "validation errors"}, status: :unprocessable_entity
         end
+    end
         
         def heropower_params
             params.permit(:strength, :power_id, :hero_id)
